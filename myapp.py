7778794d5ede473f4f4e1a20e64f9090f044cc15
ronicse <roni_cse@yahoo.com>
2025-08-18 01:02:09 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -10,4 +10,18 @@ st.subheader("Data Science with Dr. MS Rahman")
 
 # Sidebar
 st.sidebar.header("Upload CSV Data or Use Sample")
-st.sidebar.checkbox("Use example dataset")
+use_example = st.sidebar.checkbox("Use example dataset")
+
+# Load Data
+if use_example:
+  df = sns.load_dataset('iris')
+  df = df.dropna()
+  st.success("Loaded sample dataset: 'IRIS'")
+else:
+  uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=['csv'])
+  if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+  else:
+    st.warning("Please upload a CSV file or use the example dataset.")
+    st.stop()
+  
