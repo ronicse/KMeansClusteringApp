84cdfcfebb207ee19bb7869c5e236daf1dc0f2b7
ronicse <roni_cse@yahoo.com>
2025-08-18 00:56:20 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -7,3 +7,7 @@ from sklearn.cluster import KMeans
 
 st.title("KMeans Clustering App")
 st.subheader("Data Science with Dr. MS Rahman")
+
+# Sidebar
+st.sidebar.header("Upload CSV Data or Use Sample")
+st.sidebar.checkbox("Use example dataset")
