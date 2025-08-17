b3df6595398685bb765464e46b1f40e67894f0a6
ronicse <roni_cse@yahoo.com>
2025-08-18 00:50:43 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
+st.title("KMeans Clustering App")
+st.subheader("Data Science with Dr. MS Rahman")
